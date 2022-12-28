518eefacad18728a04ef1bac0954b831e4a90b94
Junho Kim <junho4623@gmail.com>
2022-12-28 18:14:05 +0900

    nltk package applications

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 # push to origin on github repository
 
 print("hello world")
+
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download()

c3b54b1f2d0fa8490fe96581c5745bba75658742
Junho Kim <junho4623@gmail.com>
2022-12-29 01:45:09 +0900

    statistical method for word distribution

---
+++ common/util.py
@@ -0,0 +1,48 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))) # access parent directory
+from common.np import *
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
+
+    word_to_id = {}
+    id_to_word = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    corpus = np.array([word_to_id[w] for w in words])
+
+    return corpus, word_to_id, id_to_word
+
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    '''
+    parameters
+    corpus: corpus in ID form
+    vocab_size: number of different words
+    window_size: window
+    
+    return:
+    co-occurrence matrix
+    '''
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
